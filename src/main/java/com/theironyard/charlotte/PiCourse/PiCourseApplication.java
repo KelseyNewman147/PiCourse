b6333941bc03d7a791bc6eb2d819e89eacd5253e
Jake T <jaketepper@gmail.com>
2017-05-21 19:13:37 -0400

    first commit

---
+++ src/main/java/com/theironyard/charlotte/PiCourse/PiCourseApplication.java
@@ -0,0 +1,12 @@
+package com.theironyard.charlotte.PiCourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiCourseApplication.class, args);
+	}
+}
